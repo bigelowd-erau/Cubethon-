e7016e294b2732b4a8a49e42ec52f9550ad6ac24
bigelowd-erau <bigelowd@my.erau.edu>
2021-09-13 00:12:21 -0700

    Added Profiler Project Stuff

--- Cubethon++/Assets/Scripts/PlayerCollision.cs
+++ Cubethon++/Assets/Scripts/PlayerCollision.cs
@@ -29,7 +29,27 @@ public class PlayerCollision : MonoBehaviour
             movement.enabled = false;
             collision.collider.GetComponent<MeshRenderer>().material.color = Color.red;
             FindObjectOfType<GameManager>().EndGame();
-
         }
     }
+    /////////////////////////////////////////////////
+   /////////////Poorly Optimized Function////////////
+    ////////////////////////////////////////////////
+    /*
+    private void OnCollisionStay(Collision collision)
+    {
+        //iff the collider is a floor
+        if (collision.collider.tag == "Floor")
+        {
+            //if the player's rotation is not the same as the floor
+            //each floor is set at 60 degree intervals
+            if (movement.playerRotation != collision.collider.transform.rotation.z)
+            {
+                //set player rotation to the floor's rotation
+                movement.playerRotation = collision.collider.transform.rotation.eulerAngles.z;
+                //change the gravity towards the direction of the players new rotation.
+                playerGravity.ChangeGravity(movement.playerRotation);
+                Debug.Log("Changed gravity towards direction of new floor");
+            }
+        }
+    }*/
 }
